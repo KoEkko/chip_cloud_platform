c7e0d32b1e3d2f4f3d34ee08760cceef0306c326
KoEkko <2251930460@qq.com>
2023-07-13 13:11:37 +0800

    feat:permission自定义指令

--- src/store/modules/permission.ts
+++ src/store/modules/permission.ts
@@ -4,13 +4,23 @@ import type { RouteRecordRaw } from "vue-router";
 import { asyncRoutes, constantRoutes } from "../../router";
 import { store } from "..";
 
-// 判断是否有权限
+/**
+ * 判断是否有权限
+ * @param roles 后端获取用户的roles信息
+ * @param route 动态路由中的其中一个路由信息
+ * @returns Boolean 是否有这个路由的权限
+ */
 const hasPermission = (roles: string[], route: RouteRecordRaw) => {
 	const routeRoles: string[] | undefined = route.meta?.roles as string[] | undefined;
 	return routeRoles ? roles.some((role) => routeRoles.includes(role)) : true; // 为空的话设置默认的角色
 };
 
-// 根据角色筛选路由
+/**
+ * 根据角色role筛选路由
+ * @param routes 前端配置的动态路由信息 asyncRoutes
+ * @param roles 后端获取用户的roles信息
+ * @returns 改用户所拥有的权限路由
+ */
 const filterAsyncRoutes = (routes: RouteRecordRaw[], roles: string[]) => {
 	const res: RouteRecordRaw[] = [];
 	routes.forEach((route) => {
