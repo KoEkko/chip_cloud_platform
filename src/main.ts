021b416ba5b0935f4d3745a55e49740a674e345f
KoEkko <2251930460@qq.com>
2023-07-13 13:12:37 +0800

    feat:permission自定义指令

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import registerStore from "./store";
 import router from "./router";
+import { loadDirective } from "./directives";
 
 const app = createApp(App);
+
+loadDirective(app);
+
 app.use(registerStore);
 app.use(router);
 app.mount("#app");
