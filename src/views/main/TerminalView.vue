f591c80671a2979cfe09674da88eaf7ddd282fb8
KoEkko <2251930460@qq.com>
2023-07-17 22:32:41 +0800

    feat:websocket link-to terminal

--- src/views/main/TerminalView.vue
+++ src/views/main/TerminalView.vue
@@ -9,6 +9,24 @@ import { onMounted, ref } from "vue";
 import { Terminal } from "xterm";
 import { FitAddon } from "xterm-addon-fit";
 
+// 创建 WebSoctket 并指定一个地址
+let ws = new WebSocket("ws://localhost:8080");
+
+// 是否连接成功
+ws.onopen = () => {
+	console.log("连接成功");
+};
+
+// 监听服务端发送过来的消息
+ws.onmessage = (messObj) => {
+	console.log("发送过来的消息", messObj);
+};
+
+// 连接失败
+ws.onerror = () => {
+	console.log("连接失败");
+};
+
 let terminal = ref(
 	new Terminal({
 		rows: 40,
@@ -24,7 +42,7 @@ let terminal = ref(
 	})
 );
 const fitAddon = new FitAddon();
-
+let inputText = ref<string>("");
 // 换行并输入起始符 $
 const prompt = () => {
 	terminal.value.write("\r\n\x1b[33m$\x1b[0m ");
@@ -38,10 +56,14 @@ const runFakeTerminal = () => {
 	terminal.value.onKey((e) => {
 		// 能够输入的按键
 		const printable = !e.domEvent.altKey && !e.domEvent.ctrlKey && !e.domEvent.metaKey;
+		// 获取键盘信息
 		let keyDown = e.domEvent.key;
 		// 特殊按键的处理
 		if (keyDown === "Enter") {
-			console.log(keyDown);
+			// 清空上一次的命令
+			ws.send(inputText.value);
+			inputText.value = "";
+			// 发送信息到服务端
 			prompt();
 		} else if (keyDown === "Backspace") {
 			// back 删除的情况
@@ -50,6 +72,7 @@ const runFakeTerminal = () => {
 				terminal.value.write("\b \b");
 			}
 		} else if (printable) {
+			inputText.value += keyDown;
 			// 不是回车和删除就是输入，写入 terminal
 			terminal.value.write(e.key);
 		}
