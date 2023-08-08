c7eabaa7c598d8f3c4c638769e006fdc476a2571
KoEkko <2251930460@qq.com>
2023-08-08 23:26:47 +0800

    test:render performance

--- src/views/main/layout/content/CanvasView.vue
+++ src/views/main/layout/content/CanvasView.vue
@@ -5,10 +5,10 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from "vue";
+import { onMounted, ref } from "vue";
 import * as PIXI from "pixi.js";
-import { throttle } from "lodash-es";
-import { useTerminalStore } from "../../../../store/modules/getTerminal";
+// import { throttle } from "lodash-es";
+// import { useTerminalStore } from "../../../../store/modules/getTerminal";
 
 const wrap = ref<HTMLBodyElement>();
 const container = ref<HTMLBodyElement>();
@@ -19,45 +19,53 @@ const container = ref<HTMLBodyElement>();
 // }
 
 // defineExpose({ handleCanvasClear });
-const terminalStore = useTerminalStore();
-const commandData = terminalStore.getResult();
-console.log(commandData);
-onMounted(() => {
+// const terminalStore = useTerminalStore();
+// const commandData = terminalStore.getResult();
+onMounted(async () => {
 	const app = new PIXI.Application({
 		background: "#000000",
 		autoDensity: true,
 		resolution: window.devicePixelRatio,
 		width: wrap.value?.clientWidth,
 		height: wrap.value?.clientHeight,
+		antialias: true,
 	});
-
-	const url = ref("../../../../../public/vite.svg");
-	setTimeout(() => {
-		url.value = "../../../../../public/kxy.png";
-	}, 2000);
 	wrap.value?.appendChild(app.view as any);
-	let texturePromise = PIXI.Assets.load(url.value);
-	watch(
-		url,
-		(newValue) => {
-			texturePromise = PIXI.Assets.load(newValue);
-			texturePromise.then((resolveTexture) => {
-				const bunny = PIXI.Sprite.from(resolveTexture);
-				bunny.anchor.set(0.5);
-				bunny.x = app.screen.width / 2;
-				bunny.y = app.screen.height / 2;
-				function resizeHandler() {
-					app.renderer.resize(wrap.value!.clientWidth, wrap.value!.clientHeight);
-					bunny.x = app.screen.width / 2;
-					bunny.y = app.screen.height / 2;
-				}
-				const throttle_Resize = throttle(resizeHandler, 200);
-				window.addEventListener("resize", throttle_Resize);
-				app.stage.addChild(bunny);
-			});
-		},
-		{ immediate: true }
-	);
+	const graphics = new PIXI.Graphics();
+	graphics.lineStyle(1, 0xffffff, 1);
+	const width = app.screen.width;
+	const height = app.screen.height;
+	const cols = Math.ceil(Math.floor(width / 5));
+	const rows = Math.ceil(Math.floor(height / 5));
+	const gapArr = [];
+	for (let i = 0; i < 5; i++) {
+		const start = performance.now();
+		for (let i = 0; i < 100000; i++) {
+			graphics.drawRect((i % cols) * 5, Math.floor(i / rows) * 5, i % 5, i % 5);
+		}
+		const end = performance.now();
+		const gap = Number((end - start).toFixed(5));
+		gapArr.push(gap);
+
+		graphics.clear();
+	}
+	const total = gapArr.reduce((prev, curr) => {
+		return prev + curr;
+	}, 0);
+	console.log(total / 5);
+	// const sheet = await PIXI.Assets.load("");
+	// const bunny = new PIXI.Sprite(sheet);
+	// bunny.anchor.set(0.5);
+	// bunny.x = app.screen.width / 2;
+	// bunny.y = app.screen.height / 2;
+	// function resizeHandler() {
+	// 	app.renderer.resize(wrap.value!.clientWidth, wrap.value!.clientHeight);
+	// 	bunny.x = app.screen.width / 2;
+	// 	bunny.y = app.screen.height / 2;
+	// }
+	// const throttle_Resize = throttle(resizeHandler, 200);
+	// window.addEventListener("resize", throttle_Resize);
+	app.stage.addChild(graphics);
 });
 </script>
 
