11de1ce50b5ec1c89aab77434420b1e24010aa2a
KoEkko <2251930460@qq.com>
2023-08-24 16:42:08 +0800

    fix:all scale

--- src/views/main/layout/content/CanvasView.vue
+++ src/views/main/layout/content/CanvasView.vue
@@ -22,12 +22,13 @@ const props = defineProps<{
 }>();
 // zoom 是放缩比， zoom 越大 step越大
 const { checkedOptions, zoom } = toRefs(props);
+let totalOffset = { x: 0, y: 0 };
+
 watch(checkedOptions, () => {
 	updateHiddenItems();
 	renderShapes();
 });
 watch(zoom, (newZoom) => {
-	scalePoint(newZoom);
 	updateRuler(newZoom, totalOffset);
 });
 
@@ -39,6 +40,50 @@ const mainApp = new PIXI.Application({
 	antialias: true,
 });
 const container = new PIXI.Container();
+const zoomRef = ref(zoom.value);
+const emits = defineEmits(["onMouseWheel"]);
+// 按照中心点来缩放
+const scaleCenter = () => {
+	const { width, height } = mainApp.screen;
+	const { x, y } = container;
+	const prevScale = zoomRef.value;
+	zoomRef.value = zoom.value;
+	const dx = (width - x) * (1 - zoom.value / prevScale);
+	const dy = (height - y) * (1 - zoom.value / prevScale);
+	container.scale.set(zoom.value);
+	container.x += dx;
+	container.y += dy;
+	totalOffset.x -= dx;
+	totalOffset.y -= dy;
+};
+defineExpose({ scaleCenter });
+// 按照鼠标位置滚轮缩放
+const onMouseWheel = (e: PIXI.FederatedWheelEvent) => {
+	// 获取鼠标的位置
+	const { x: gx, y: gy } = e.global;
+	// 获取container的位置
+	const { x: cx, y: cy } = container;
+	// 最后一次的zoom值
+	const prevScale = zoom.value;
+	// 鼠标滚轮的方向
+	const deltaY = e.deltaY;
+	zoomRef.value += deltaY > 0 ? 0.1 : -0.1;
+	// 最小的zoom值
+	if (zoomRef.value < 0.2) {
+		zoomRef.value = 0.2;
+	}
+	// 更新zoom的值
+	emits("onMouseWheel", zoomRef.value);
+	// 计算偏移值，根据变换矩阵来计算
+	const dx = (gx - cx) * (1 - zoomRef.value / prevScale);
+	const dy = (gy - cy) * (1 - zoomRef.value / prevScale);
+	container.scale.set(zoomRef.value);
+	// container做相对应的移动
+	container.x += dx;
+	container.y += dy;
+	totalOffset.x -= dx;
+	totalOffset.y -= dy;
+};
 const setupMainApp = () => {
 	const width = main.value!.clientWidth;
 	const height = main.value!.clientHeight;
@@ -47,6 +92,7 @@ const setupMainApp = () => {
 	mainApp.stage.eventMode = "dynamic";
 	// 确保舞台覆盖整个场景
 	mainApp.stage.hitArea = mainApp.screen;
+	mainApp.stage.on("wheel", onMouseWheel);
 	mainApp.stage.on("pointerdown", onDragStart);
 	mainApp.stage.on("pointermove", throttleOnDrag);
 	mainApp.stage.on("pointerup", onDragEnd);
@@ -59,7 +105,6 @@ const setupMainApp = () => {
 
 let isDragging = false;
 let lastPosition = new PIXI.Point();
-let totalOffset = { x: 0, y: 0 };
 const onDragStart = (e: PIXI.FederatedPointerEvent) => {
 	e.preventDefault();
 	isDragging = true;
@@ -70,6 +115,7 @@ const onDragStart = (e: PIXI.FederatedPointerEvent) => {
 const onDrag = (e: PIXI.FederatedPointerEvent) => {
 	if (isDragging) {
 		const { x: globalX, y: globalY } = e.global;
+		// 滑动的距离
 		const dx = (globalX - lastPosition.x) / zoom.value;
 		const dy = (globalY - lastPosition.y) / zoom.value;
 		totalOffset.x += dx;
@@ -124,7 +170,7 @@ const setupLeftApp = () => {
  */
 const getStepByZoom = (zoom: number) => {
 	const steps = [1, 2, 5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000];
-	const step = 50 * zoom;
+	const step = 50 / zoom;
 	for (let i = 0, len = steps.length; i < len; i++) {
 		if (steps[i] >= step) return steps[i];
 	}
@@ -155,11 +201,6 @@ type Positon = {
 	y: number;
 };
 
-// 缩放中心点
-const scalePoint = (newZoom: number) => {
-	container.pivot.set(0.5);
-	container.scale.set(newZoom);
-};
 // 减少频繁计算 main 的client
 const getMainRectBound = computed(() => {
 	return {
@@ -167,23 +208,34 @@ const getMainRectBound = computed(() => {
 		height: main.value?.clientHeight,
 	};
 });
-// 标尺更新
-const updateRuler = (zoom: number, position: Positon = { x: 0, y: 0 }) => {
+const viewport = {
+	x: 0,
+	y: 0,
+	width: 0,
+	height: 0,
+};
+const getSceneCoords = (position: Positon = { x: 0, y: 0 }) => {
 	const RectBound = getMainRectBound;
 	const { width, height } = RectBound.value;
-	const viewport = {
-		x: position.x,
-		y: position.y,
-		width,
-		height,
-	};
+	viewport.x = position.x / zoom.value;
+	viewport.y = position.y / zoom.value;
+	viewport.width = width!;
+	viewport.height = height!;
+};
+/**
+ * 标尺更新
+ * @param zoom 缩放比
+ * @param position 画布所在的位置
+ */
+const updateRuler = (zoom: number, position: Positon = { x: 0, y: 0 }) => {
+	getSceneCoords(position);
 	// X 坐标的标尺
 	rulerContainerX.removeChildren(); // 清空之前的文本元素
 	rulerGraphicsX.clear();
 
 	const newStep = getStepByZoom(zoom);
 	const startMarkX = getClosestVal(viewport.x, newStep);
-	const endMarkX = getClosestVal(Math.ceil(viewport.x + viewport.width! / zoom), newStep);
+	const endMarkX = getClosestVal(viewport.x + viewport.width / zoom, newStep);
 	rulerGraphicsX.lineStyle(1, setting.rulerMarkStroke);
 	for (let x = startMarkX; x <= endMarkX; x += newStep) {
 		// 刻度线,刻度线不能直接在场景中绘制，因为缩放变换会导致线的粗细变化
--- src/views/main/layout/content/ContentView.vue
+++ src/views/main/layout/content/ContentView.vue
@@ -1,7 +1,12 @@
 <template>
 	<ToolsArea @scale-control="scaleHandler" />
 	<div class="content-box">
-		<CanvasView ref="canvasView" :checked-options="checkedOptions" :zoom="zoomValue" />
+		<CanvasView
+			ref="canvasView"
+			:checked-options="checkedOptions"
+			:zoom="zoomValue"
+			@on-mouse-wheel="mouseWheelHandler"
+		/>
 		<ControlTools @on-check-box-click="onCheckBoxChange" />
 	</div>
 </template>
@@ -12,14 +17,22 @@ import CanvasView from "./CanvasView.vue";
 import ControlTools from "./ControlTools.vue";
 import ToolsArea from "./ToolsArea.vue";
 
+const canvasView = ref<InstanceType<typeof CanvasView>>();
 const checkedOptions: Ref<string[]> = ref([]);
 const onCheckBoxChange = (options: string[]) => {
 	checkedOptions.value = Array.from(options);
 };
 
 let zoomValue: Ref<number> = ref(1);
+
 const scaleHandler = (scale: number) => {
+	// 修改全局的zoom
 	zoomValue.value = scale;
+	canvasView.value?.scaleCenter();
+};
+
+const mouseWheelHandler = (newZoom: number) => {
+	zoomValue.value = newZoom;
 };
 </script>
 
